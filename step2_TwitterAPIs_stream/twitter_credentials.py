90f4953f7e8d97d27ab57edca56407c0945ea9ec
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-12-25 20:09:55 -0500

    Update twitter_credentials.py

--- step2_TwitterAPIs_stream/twitter_credentials.py
+++ step2_TwitterAPIs_stream/twitter_credentials.py
@@ -1,7 +1,8 @@
 # !/usr/bin/env/python3
 # Copyright 2019 YueLiu liuyue2@bu.edu
-# Program Goal: get twitter credential keys and secrets
+# Program Goal: get twitter apps keys and tokens
 # https://developer.twitter.com/en/apps
+# the access level is read and write
 
 consumer_key = ''
 consumer_secret = ''
