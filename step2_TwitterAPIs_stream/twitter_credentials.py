a2f502b6ef70d7cdc9b315c57006339983186d43
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-12-25 20:06:35 -0500

    Update and rename 1tweepy_get_stream.py to twitter_credentials.py

---
+++ step2_TwitterAPIs_stream/twitter_credentials.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env/python3
+# Copyright 2019 YueLiu liuyue2@bu.edu
+# Program Goal: get twitter credential keys and secrets
+# https://developer.twitter.com/en/apps
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
