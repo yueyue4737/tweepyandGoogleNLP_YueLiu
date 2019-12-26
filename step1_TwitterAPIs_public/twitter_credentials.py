ffcd9a9fb78d2a5ee6dca0b489dafbe9989741f5
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-12-25 20:50:05 -0500

    Update twitter_credentials.py

--- step1_TwitterAPIs_public/twitter_credentials.py
+++ step1_TwitterAPIs_public/twitter_credentials.py
@@ -1,6 +1,7 @@
 # !/usr/bin/env/python3
 # Copyright 2019 YueLiu liuyue2@bu.edu
 # Program Goal: get twitter apps keys and tokens
+# register a client application with Twitter in
 # https://developer.twitter.com/en/apps
 # the access level is read and write
 
