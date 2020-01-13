c1334e2523de727e83fb1659dfaff716736cf35c
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2020-01-13 00:07:42 -0500

    Update twython_get_public.py

--- step1_TwitterAPIs_public/twython_get_public.py
+++ step1_TwitterAPIs_public/twython_get_public.py
@@ -1,1 +1,27 @@
-d
+# !/usr/bin/env/python3
+# Copyright 2019 YueLiu liuyue2@bu.edu
+# Program Goal: get the public tweets from a famous organization user
+# @kaggle
+# Help understand Error condition
+
+import twitter_credentials
+import twython
+
+def get_authentication():
+    api = twython.Twython(twitter_credentials.consumer_key,
+                          twitter_credentials.consumer_secret,
+                          twitter_credentials.access_token,
+                          twitter_credentials.access_token_secret)
+    return api
+
+def get_public_tweets(api,screen_name):
+    try:
+        user_timeline = api.get_user_timeline(screen_name=screen_name)
+    except twython.TwythonError as e:
+        print(e)
+    print(user_timeline)
+
+api = get_authentication()
+screen_name = 'kaggle'
+public_tweets = get_public_tweets(api, screen_name)
+print(public_tweets)
